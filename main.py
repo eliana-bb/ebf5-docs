af1b94c14575285af0ce5227fb2b2562c7fb31b0
eliana-bb <eliana.2k4@gmail.com>
2024-11-10 19:12:51 -0600

    more dump system

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ import json
 NOW = datetime.datetime.now(datetime.UTC).strftime("%Y%m%d%H%M%S%f")[:17]
 
 stats = ("HP", "attack", "defence", "magicAttack", "magicDefence", "accuracy", "evade")
-for stat in stats:
-    print("\n\n")
-    for equip in data.equips.values():
-        equip_name = data.equip_names[equip["SID"]][0]
-        print(equip_name, end=":")
-        for k, v in enumerate(equip[stat]):
-            print(v, end="")
-            if k != len(equip[stat])-1:
-                print(",", end="")
-            else:
-                print()
+for equip in data.equips.values():
+    equip_name = data.equip_names[equip["SID"]][0]
+    has_status = "statusEffect" in equip and equip["statusEffect"] not in (None, "Status.NONE")
+    has_buff = "buffEffect" in equip and equip["buffEffect"] not in (None, "Stats.NONE")
+    has_specials = "specials" in equip and equip["specials"] not in ([], [None])
+    if not has_specials:
+        print(equip_name)
