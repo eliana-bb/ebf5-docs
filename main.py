30997cf52ba235e19d7dfa32789d815df12b2344
eliana-bb <eliana.2k4@gmail.com>
2024-11-11 12:40:41 -0600

    work on unleashes

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import json
 NOW = datetime.datetime.now(datetime.UTC).strftime("%Y%m%d%H%M%S%f")[:17]
 
 stats = ("HP", "attack", "defence", "magicAttack", "magicDefence", "accuracy", "evade")
-for equip_name, equip in data.equips.items():
-    if "specials" not in equip:
-        continue
-    for special in equip["specials"]:
-        if special is None:
-            continue
-        if special[0] == "Equip.CAST":
-            print(equip_name, end=":")
-            skill_name = data.skill_names[special[1][7:]]
-            print(skill_name, end=";")
-            print(special[2])
+with open("data/Spells.as") as infile:
+    upgrade_chains = re.findall(r" (\w+)\.upgradesInto = (\w+);", infile.read())
+
+upgrade_chains = dict(upgrade_chains)
+
+for skill_id, skill_name in data.skill_names.items():
+    print(skill_name, end=":")
+    current_skill_id = skill_id
+    while current_skill_id in upgrade_chains:
+        current_skill_id = upgrade_chains[current_skill_id]
+    print(data.skill_names[current_skill_id])
