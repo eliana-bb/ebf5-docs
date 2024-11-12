dcd4d389aae82ec431c35d2adffd99f5b7989e0a
eliana-bb <eliana.2k4@gmail.com>
2024-11-12 14:21:45 -0600

    dump it out

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ import json
 NOW = datetime.datetime.now(datetime.UTC).strftime("%Y%m%d%H%M%S%f")[:17]
 
 stats = ("HP", "attack", "defence", "magicAttack", "magicDefence", "accuracy", "evade")
-with open("data/Spells.as") as infile:
-    upgrade_chains = re.findall(r" (\w+)\.upgradesInto = (\w+);", infile.read())
 
-upgrade_chains = dict(upgrade_chains)
-
-for skill_id, skill_name in data.skill_names.items():
-    print(skill_name, end=":")
-    current_skill_id = skill_id
-    while current_skill_id in upgrade_chains:
-        current_skill_id = upgrade_chains[current_skill_id]
-    print(data.skill_names[current_skill_id])
+for equip_name, equip in data.equips.items():
+    if "specials" not in equip:
+        continue
+    if equip["specials"] is None:
+        continue
+    for lv, special in enumerate(equip["specials"]):
+        if special is None:
+            continue
+        if special[0] == "Equip.STATUS":
+            print(equip_name, end=":")
+            print(special[1][7:].replace("_", " ").title().replace("Autolife", "Auto-Revive"), end=";")
+            print(special[2], end=";")
+            print(special[3], end=";")
+            print(lv+1)
